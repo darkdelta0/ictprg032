7d344e545714020b17ddecf5491dea52c9edf7e5
EC2 Default User <ec2-user@ip-172-31-34-60.ap-southeast-2.compute.internal>
2024-04-19 04:27:03 +0000

    19Apr

--- backup.py
+++ backup.py
@@ -4,8 +4,72 @@ import sys
 import os
 import pathlib
 import shutil
+import smtplib
 from datetime import datetime
-from backupcfg import jobs, dstPath
+from backupcfg import jobs, dstPath, logPath, smtp
+
+
+
+def sendEmail(errorMessage, dateTimeStamp):
+  
+  email = 'To: ' + smtp["recipient"] + '\n' + 'From: ' + smtp["sender"] + '\n' + 'Subject: Log\n\n' + errorMessage + '\n'
+
+    # connect to email server and send email
+  try:
+      smtp_server = smtplib.SMTP(smtp["server"], smtp["port"])
+      smtp_server.ehlo()
+      smtp_server.starttls()
+      smtp_server.ehlo()
+      smtp_server.login(smtp["user"], smtp["password"])
+      smtp_server.sendmail(smtp["sender"], smtp["recipient"], email)
+      smtp_server.close()
+  except Exception as e:
+      print("ERROR: An error occurred.")
+  pass
+
+def Errorlogging(errorMessage, dateTimeStamp):
+    try:
+        file = open(logPath, "a")
+        file.write(f"{errorMessage} {dateTimeStamp}\n")
+        file.close()
+        
+        print("error has been found, proceeding to log")
+        
+    except FileNotFoundError:
+        print("ERROR: File does not exist.")
+    except IOError:
+        print("ERROR: File is not accessible.")
+
+def error(errorMessage, dateTimeStamp):
+  print(errorMessage)#print error message on screen 
+  
+  Errorlogging(f"FAILURE {errorMessage}", dateTimeStamp)
+  sendEmail(f"FAILURE {errorMessage}", dateTimeStamp)
+  #write failure to log file
+  
+  #email error message to administrator
+
+
+def Succesfullogging(errorMessage, dateTimeStamp):
+    try:
+        file = open(logPath, "a")
+        file.write(f"{errorMessage} {dateTimeStamp}\n")
+        file.close()
+        
+        print("Succes: proceeding to log")
+        
+    except FileNotFoundError:
+        print("ERROR: File does not exist.")
+    except IOError:
+        print("ERROR: File is not accessible.")
+
+def success(Message, dateTimeStamp):
+  print(Message)#print error message on screen 
+  
+  Succesfullogging(f"Success {Message}", dateTimeStamp)
+  sendEmail(f"Success {Message}", dateTimeStamp)
+  
+
 
 def main():
     #code for the time stamp for backups
@@ -13,21 +77,20 @@ def main():
     argCount = len(sys.argv)
     #code to find 
     if argCount != 2:
-        print("ERROR: jobname missing from command line.")
+        error("ERROR: jobname missing from command line. ", dateTimeStamp) 
     else:
         jobName = sys.argv[1]
         if jobName not in jobs:
-            print(f"ERROR: jobname {jobName} not defined")
+            error(f"ERROR: jobname {jobName} not defined. ", dateTimeStamp) 
         else:
             srcPath=jobs[jobName]
             if not os.path.exists(srcPath):
-                print(f"ERROR: Source path {srcPath} does not exist")
+                error(f"ERROR: Source path {srcPath} does not exist", dateTimeStamp)
             else:
                 if not os.path.exists(dstPath):
-                    print(f"ERROR: Destination path {dstPath} does not exist")
+                  error(f"ERROR: Destination path {dstPath} does not exist", dateTimeStamp)
                 else:
                     srcDetails = pathlib.PurePath(srcPath)
-                    
                     dstLoc = dstPath + "/" + srcDetails.name + "-" + dateTimeStamp
                     
                     if pathlib.Path(srcPath).is_dir():
@@ -35,6 +98,10 @@ def main():
                       
                     else:
                       shutil.copy2(srcPath, dstLoc)
+                      success("backup created ", dateTimeStamp)
+                      
+                      
+                    #write to log file
                     
 if __name__ == '__main__':
     main()
--- backupcfg.py
+++ backupcfg.py
@@ -2,3 +2,12 @@ jobs = {'job17': '/home/ec2-user/environment/ictprg032/dir1//file2.dat',
         'job18':'/home/ec2-user/environment/ictprg032/dir1/file3.dat'}
         
 dstPath = '/home/ec2-user/environment/ictprg032/backups/'
+
+logPath = '/home/ec2-user/environment/ictprg032/backup.log'
+
+smtp = {"sender": "maxthedood1@gmail.com",    # elasticemail.com verified sender
+        "recipient": "30025404@students.sunitafe.edu.au", # elasticemail.com verified recipient
+        "server": "smtp.elasticemail.com",      # elasticemail.com SMTP server
+        "port": 2525,                           # elasticemail.com SMTP port
+        "user": "maxthedood1@gmail.com",      # elasticemail.com user
+        "password": "AC3A4E616ED040069A44CA6229010FFAC13B"}     # elasticemail.com password #AC3A4E616ED040069A44CA6229010FFAC13B
