cd4e8d34169f5868271f611f143b5b3b137b6418
EC2 Default User <ec2-user@ip-172-31-34-60.ap-southeast-2.compute.internal>
2024-08-15 03:23:29 +0000

    15/082024

--- backup.py
+++ backup.py
@@ -10,14 +10,15 @@ from backupcfg import jobs, dstPath, logPath, smtp
 
 #Author: Max Cosby
 #Version:1,2
-#program name
-#description
-#copyright
+#Name: Backup.py
+#Copyright: Max Cosby 2024
+#This program backups files and directorys, Each backup is defined as "job" in backupcfg.py, To run the program type in "backup.py" with any jobname that is defined in backupcfg.py
 
 def sendEmail(errorMessage, dateTimeStamp):
+   # this function connects to a email server and sends an email
   email = 'To: ' + smtp["recipient"] + '\n' + 'From: ' + smtp["sender"] + '\n' + 'Subject: Log\n\n' + errorMessage + '\n'
 
-    # connect to email server and send email
+   
   try:
       smtp_server = smtplib.SMTP(smtp["server"], smtp["port"])
       smtp_server.ehlo()
@@ -31,7 +32,7 @@ def sendEmail(errorMessage, dateTimeStamp):
   pass
 
 def Errorlogging(errorMessage, dateTimeStamp):
-  #this code is used to log
+  #this function opens a file and writes and error message into it and then proceeds to close it
     try:
         file = open(logPath, "a") #this is used for to find the document and to be able to use it
         file.write(f"{errorMessage} {dateTimeStamp}\n") #this code is writing in the document
@@ -46,6 +47,7 @@ def Errorlogging(errorMessage, dateTimeStamp):
         print("ERROR: File is not accessible.")
 
 def error(errorMessage, dateTimeStamp):
+  #this functions  write an error message into terminal and then into the log file and finally send an email 
   print(errorMessage)#print error message on screen 
   
   Errorlogging(f"FAILURE {errorMessage}", dateTimeStamp)
@@ -56,6 +58,7 @@ def error(errorMessage, dateTimeStamp):
 
 
 def Succesfullogging(errorMessage, dateTimeStamp):
+  #this function opens a file and writes and success message into it and then proceeds to close it
     try:
         file = open(logPath, "a")
         file.write(f"{errorMessage} {dateTimeStamp}\n")
@@ -69,7 +72,8 @@ def Succesfullogging(errorMessage, dateTimeStamp):
         print("ERROR: File is not accessible.")
 
 def success(Message, dateTimeStamp):
-  print(Message)#print error message on screen 
+  #this functions  write an success message into terminal and then into the log file and finally send an email 
+  print(Message)#print success message on screen 
   
   Succesfullogging(f"Success {Message}", dateTimeStamp)
   sendEmail(f"Success {Message}", dateTimeStamp)
@@ -77,27 +81,31 @@ def success(Message, dateTimeStamp):
 
 
 def main(): 
+  #this function checks if the input in the command line is valid  which is then backed up and then is logged in "backup.log"
     #code for the time stamp for backups
     dateTimeStamp = datetime.now().strftime("%Y%m%d-%H%M%S")
     argCount = len(sys.argv)
-    #code to find 
-    if argCount != 2:
+    #Checks to see if jobname is specified
+    if argCount != 2: 
         error("ERROR: jobname missing from command line. ", dateTimeStamp) 
     else: #checks to see if the input in the console alighns with the jobs in backupcfg.py
         jobName = sys.argv[1]
+        #checks if jobname is defined 
         if jobName not in jobs:
             error(f"ERROR: jobname {jobName} not defined. ", dateTimeStamp) 
         else:
             srcPath=jobs[jobName]
+            #checks if Source Path exists
             if not os.path.exists(srcPath):
                 error(f"ERROR: Source path {srcPath} does not exist", dateTimeStamp)
             else:
+              #checks if destination path exists
                 if not os.path.exists(dstPath): #sees if the path is actually a thing
                   error(f"ERROR: Destination path {dstPath} does not exist", dateTimeStamp)
                 else:
                     srcDetails = pathlib.PurePath(srcPath)
                     dstLoc = dstPath + "/" + srcDetails.name + "-" + dateTimeStamp
-                    
+                    #Copys either a file or directory to the destination path
                     if pathlib.Path(srcPath).is_dir():
                       shutil.copytree(srcPath, dstLoc)
                       
